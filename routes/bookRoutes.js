42344556268c81b7c508868c7607367f8aa5b0cd
88chinu <chinmaya.pa42@gmail.com>
2024-09-18 13:14:10 +0000

    complete express l3

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 const bookController = require('../controllers/bookController.js');
 
-router.post('/books', bookController.createBook);
-router.get('/books',bookController.getAllBooks);
+router.post('/books', bookController.createBook)
+router.get('/books',bookController.getAllBooks)
+router.get('/books/:id',bookController.getBookById)
+router.put('/books/:id', bookController.updateBook)
+router.delete('/books/:id', bookController.deleteBook)
 
 module.exports = router;
