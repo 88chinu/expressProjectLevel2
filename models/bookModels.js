00c8bb70d736fe28d6da35adc39a0b640ceb2e84
88chinu <chinmaya.pa42@gmail.com>
2024-11-09 17:56:53 +0000

    expressLevel2

--- models/bookModels.js
+++ models/bookModels.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose')
 const bookSchema = new mongoose.Schema({
     title:{
         type: String,
-        require: true,
-        unique: true
+        require: true, //add require to make it a mandatory parameter
+        unique: true  //add unique constrain to prevent duplicates
     },
     author:{
-        type: String, //add require to make it a mandatory parameter
-        require: true //add unique constrain to prevent duplicates
+        type: String, 
+        require: true 
     }
 })
 
