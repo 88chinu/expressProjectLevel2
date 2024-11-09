af5e9785238f5e90eee1234979921d0f34aa0d41
88chinu <chinmaya.pa42@gmail.com>
2024-11-09 17:48:08 +0000

    expressLevel2

--- config/db.js
+++ config/db.js
@@ -4,8 +4,9 @@ const connectDB = async () => {
     try {
         await mongoose.connect('mongodb://localhost:27017/mydatabase');
         console.log('Connected to MongoDB');
-    } catch (err) {
-        console.error('Failed to connect to MongoDB', err);
+    }
+    catch(err){
+        console.error("Failed to connect to MongoDB",err);
         process.exit(1);
     }
 };
