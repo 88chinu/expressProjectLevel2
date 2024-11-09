20b0c93ff7b34b7b389c97d63b89939d270a3767
88chinu <chinmaya.pa42@gmail.com>
2024-11-09 18:04:17 +0000

    expressLevel2

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 const bookController = require('../controllers/bookController.js');
 
-router.post('/books', bookController.createBook)
-router.get('/books',bookController.getAllBooks)
-router.get('/books/:id',bookController.getBookById)
-router.put('/books/:id', bookController.updateBook)
-router.delete('/books/:id', bookController.deleteBook)
+router.post('/books', bookController.createBook) //define routes for post method 
+router.get('/books',bookController.getAllBooks) //define routes for get method 
+router.get('/books/:id',bookController.getBookById) //define routes for get by Id method 
+router.put('/books/:id', bookController.updateBook) //define routes for update by Id method 
+router.delete('/books/:id', bookController.deleteBook) //define routes for delete by Id method 
 
 module.exports = router;
